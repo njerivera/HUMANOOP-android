891222f2903bfe73520dbc79e0623fc580e377f5
njerivera <veronjeri85@gmail.com>
2019-05-19 22:04:58 +0300

    HUMANOOP

---
+++ app/src/main/java/com/example/humanoop/Human.java
@@ -0,0 +1,58 @@
+package com.example.humanoop;
+
+import android.util.Log;
+
+public class Human {
+ public String name;
+ public int age;
+ public int weight;
+
+    public Human(String name, int age, int weight) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+public void eat(){
+    Log.d("Human","I am eating food");
+}
+public void sleep() {
+        Log.d("Human","zzzzzzzzzz");
+}
+public void sleep(int hours){
+        Log.d("Human","i will sleep for "+ hours + " " +"hours");
+
+}
+public void speak(String speech) {
+        Log.d("Human",speech);
+}
+public int birthday(){
+            age = age +1;
+            return age;
+}
+
+}
+
